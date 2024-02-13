971b0cdede95b35604e2910dd7c36937eeb03759
Shubham Tiwari <shubht3303@gmail.com>
2024-02-13 23:23:28 +0530

    added : week-2

---
+++ Week2/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const port = 3000
+
+var calculateSum = function() {
+  var sum = 0;
+  for (let i = 0; i < 1000; i++) {
+    sum += i;
+  }
+  return sum;
+}
+
+function handleFirstRequest(req, res) {
+  var counter = req.query.counter;
+  var sum = 0;
+    for (let i = 0; i <= counter; i++) {
+    sum = sum+i;
+  }
+  res.send('Sum is ' + sum);
+}
+
+app.get('/handlesum', handleFirstRequest)
+
+
+function startServer() {
+  console.log(`Example app listening on port ${port}`);
+}
+app.listen(port, startServer);
+
+//app.listen runs infinitely just like setInterval and exposes the express app to the required port.
