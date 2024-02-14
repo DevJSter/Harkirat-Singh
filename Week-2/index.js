420b3a78d8e8839870312ccf9e9050b60e5cf74b
Shubham Tiwari <shubht3303@gmail.com>
2024-02-15 01:07:06 +0530

    added : used Body-Parser

--- Week-2/index.js
+++ Week-2/index.js
@@ -2,18 +2,38 @@ const express = require('express');
 const app = express();
 const port = 3000
 
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+
+// function middleware(req, res, next) {   //middleware function
+//   console.log('Middleware called' + " " +  req.headers.counter);
+//   next(); //the control wil reach the next middleware or route handler
+// }
+
+// app.use(middleware); //app.use is used to mount the middleware function
+
 var calculateSum = function(counter) {
   var sum = 0;
-  for (let i = 0; i <= counter; i++) {
-    sum += i;
+    for (let i = 0; i <= counter; i++) {
+      sum += i;
   }
   return sum;
 }
 
 function handleFirstRequest(req, res) {
-  var counter = req.query.counter;
+  // console.log(req.body); //Body parser helps to parse the data sent from the body to backend and it parses the json to simple data
+  //As using the express we can only use the the headers to send the data to the backend ,,but Body-Parser changes everything
+  var counter = req.body.counter || 10;
+  if(counter < 1000000){
+    
   var finalsum = calculateSum(counter);
   res.send('Sum is ' + finalsum);
+  }
+  else{
+    res.status(411).send("Youve sent a big number are ypu dumb?");
+}
 }
 
 app.get('/sum', handleFirstRequest)
@@ -36,7 +56,8 @@ function createUser(req, res) {
   res.send('User created');
 }
 
-app.post('/createUser', createUser);
+app.post('/createUser', handleFirstRequest);
+app.post('/create', createUser);
 
 function updateUser(req, res) {
   var user = req.body;
@@ -57,3 +78,11 @@ app.delete('/deleteUser', deleteUser);
 app.listen(port, startServer);
 
 //app.listen runs infinitely just like setInterval and exposes the express app to the required port.
+//req.headers is an object that contains the headers sent by the client.(?counter=5&name=John)  //req.query is an object that contains the query string parameters.
+
+
+
+
+// Responses from the server other than data it can also return status codes, headers and body.
+
+
