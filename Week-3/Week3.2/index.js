378a702ebce09a84c11f1c78b37e25fd42edf15b
Shubham Tiwari <shubht3303@gmail.com>
2024-03-08 01:51:36 +0530

    Week3.2-Authentication

--- Week-3/Week3.2/index.js
+++ Week-3/Week3.2/index.js
@@ -7,10 +7,28 @@ let ADMINS = [];
 let USERS = [];
 let COURSES = [];
 
-function adminAuthentication(req,res,next) {
+const adminAuthentication = (req,res,next) => {
 
+    const { username,password } = req.headers;
+    const admin = ADMINS.find(a => a.username === username && a.password === password); 
+   
+    if(admin) {
+        next();
+    } else {
+        res.status(403).json ({message : "Admin Auth failed"})
+    }
+
+}
+const UserAuthentication = (req,res,next) => {
+
+    const { username , password } = req.headers;
+    const user = USERS.find(a => a.username === username && a.password === password);
     
-
+    if(user) {
+        next();
+    } else {
+        res.status(403).json ({message : "Admin Auth failed"})
+    }
 
 }
 
@@ -27,33 +45,64 @@ app.post("/admin/signup", (req, res) => {
     res.json ({
         message : 'Admin Created succesfully'
     })
-  }
-
+  } }
   );
 
 app.post("/admin/login", adminAuthentication,(req, res) => {
   // logic to log in admin
+  res.json ({message : "Admin Login succesful"})
 });
 
-app.post("/admin/courses", (req, res) => {
+app.post("/admin/courses",adminAuthentication, (req, res) => {
   // logic to create a course
+    const course = req.body;
+    if(!course.title) {
+        return res.json(411).send({message : "Please send me the title"});
+    }
+    course.id = Date.now();
+    COURSES.push(course) ;
+    res.json({message : "Course Created Succesfully", courseID : course.id})
+   
 });
 
-app.put("/admin/courses/:courseId", (req, res) => {
-  // logic to edit a course
+app.put("/admin/courses/:courseId", adminAuthentication, (req, res) => {
+  // logic to edit a course 
+    const courseId = parseInt(res,params.courseId);
+    const course = COURSES.find(c => c.id === courseId);
+
+    if(course) {
+        Object.assign(course ,req.body);
+        res.json({ message : "Course Updated succesfully"});
+
+    }else { 
+        res.status(404).json({message : "Course not Found"});
+    }
+
 });
 
 app.get("/admin/courses", (req, res) => {
+
   // logic to get all courses
 });
 
 // User routes
 app.post("/users/signup", (req, res) => {
   // logic to sign up user
-});
+  const user = req.body;
+  const existingUser = USERS.find(a => a.username === user.username);
 
-app.post("/users/login", (req, res) => {
+  if(existingUser) {
+    res.status(403).json ({message : 'User already exists'});
+  } else { 
+    USERS.push(user);
+    res.json ({
+        message : 'Admin Created succesfully'
+    })
+  } }
+
+app.post("/users/login", UserAuthentication (req, res) => {
   // logic to log in user
+   res.json ({message : "Admin Login succesful"})
 });
 
 app.get("/users/courses", (req, res) => {
