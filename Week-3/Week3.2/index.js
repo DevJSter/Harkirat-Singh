a8166aa4ef4ca909f998bc663d5e0a3b5f938532
Shubham Tiwari <shubht3303@gmail.com>
2024-03-08 13:25:54 +0530

    added : UserAuth

--- Week-3/Week3.2/index.js
+++ Week-3/Week3.2/index.js
@@ -109,7 +109,7 @@ app.post("/users/courses/:courseId", UserAuthentication ,(req, res) => {
   const courseId = Number(req.params.courseId);
   const course = COURSES.find((c) => c.id === courseId && c.published);
   if (course) {
-    req.user.purchasedCourses;.push(courseId);
+    req.user.purchasedCourses.push(courseId);
     res.json({message : "Course Purchased Successfully"});
    }else {
     res.status(404).json({  message : "Course not found or unavailable" });
