a1ca992ef44ee24fc87e4907fdf73fa4e519c3b3
Shubham Tiwari <shubht3303@gmail.com>
2024-03-14 01:27:13 +0530

    map

--- node.js
+++ node.js
@@ -1,10 +1,17 @@
 let arr = [1,2,3,4,5,6,8];
 
-function doubled(arr) {
-for(let i = 0; i < arr.length; i++) {
-    arr[i] = arr[i]*2;
-}
-}
+// function doubled(arr) {
+// for(let i = 0; i < arr.length; i++) {
+//     arr[i] = arr[i]*2;
+// }
+// }
 
-doubled(arr);
-console.log(arr); 
+// doubled(arr);
+// console.log(arr); 
+
+const newArray = arr.map((value) => {
+    return value*2;
+});
+
+console.log(newArray);
+
