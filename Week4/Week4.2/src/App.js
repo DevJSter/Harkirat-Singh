a8e5438163181acea8591a064c45ffc3ce6b4780
Shubham Tiwari <shubht3303@gmail.com>
2024-03-22 11:43:24 +0530

    2

--- Week4/Week4.2/src/App.js
+++ Week4/Week4.2/src/App.js
@@ -1,54 +1,31 @@
-import "./App.css";
-import { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 
-function App() {
-  const [todos, setTodos] = useState([
-    {
-      title: "My first todo",
-      description: "My first todo description",
-      id: 1,
-    },
-  ]);
+const App = () => {
+  const [todoForToday, setTodoForToday] = React.useState({
+    tite: "Gym jana hai taanu",
+    description: "Shaam 6 - 7",
+    id: 1,
+  });
+  
+  let ctr = 0;
 
-  // Use useEffect instead of setInterval for side effects
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setTodos((prevTodos) => [
-        ...prevTodos,
-        {
-          title: "Khana khalo",
-          description: "Khana time se khana hai bhai",
-          id: Math.random(), // Use a unique id here
-        },
-      ]);
-    }, 2000);
-
-    // Clean up the interval to avoid memory leaks
-    return () => clearInterval(interval);
-  }, []); // Empty dependency array to run the effect only once
-
-  return (
-    <>
-      {todos.map((todo) => (
-        <div key={todo.id}>
-          <h1>{todo.title}</h1>
-          <p>{todo.description}</p>
-          <p>{todo.id}</p>
-        </div>
-      ))}
-      {/* This component seems unnecessary based on the provided code */}
-      <PersonName firstName={todos.description} lastName={todos.id} />
-    </>
-  );
+  if (ctr===0) {
+  setTimeout(() => {
+    setTodoForToday({
+      tite: "Gym jana hai taanu",
+      description: "Shaam 6 -564",
+      id: 45, 
+    });
+  }, 1000);
+  ctr = 1;
 }
 
-// Function component to display person's name
-function PersonName(props) {
   return (
     <div>
-      {props.firstName}, {props.lastName}
+      {todoForToday.tite}
+      {todoForToday.description}
     </div>
   );
-}
+};
 
 export default App;
