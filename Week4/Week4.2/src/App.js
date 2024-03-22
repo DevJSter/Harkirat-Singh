060ab93c5a33a13c95650bb74f9e36e8da579e14
Shubham Tiwari <shubht3303@gmail.com>
2024-03-22 12:37:50 +0530

    123

--- Week4/Week4.2/src/App.js
+++ Week4/Week4.2/src/App.js
@@ -6,24 +6,20 @@ const App = () => {
     description: "Shaam 6 - 7",
     id: 1,
   });
-  
-  let ctr = 0;
 
-  if (ctr===0) {
   setTimeout(() => {
     setTodoForToday({
       tite: "Gym jana hai taanu",
       description: "Shaam 6 -564",
-      id: 45, 
+      id: Math.random() * 1000,
     });
   }, 1000);
-  ctr = 1;
-}
 
   return (
     <div>
       {todoForToday.tite}
       {todoForToday.description}
+      {todoForToday.id}
     </div>
   );
 };
