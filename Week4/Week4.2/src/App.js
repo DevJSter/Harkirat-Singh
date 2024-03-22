c19a94a57ab633b1db2028156abe5cc2f3b91187
Shubham Tiwari <shubht3303@gmail.com>
2024-03-22 12:52:33 +0530

    ADDED SOME TODOS

--- Week4/Week4.2/src/App.js
+++ Week4/Week4.2/src/App.js
@@ -1,19 +1,22 @@
 import React, { useEffect } from "react";
 
 const App = () => {
-  const [todoForToday, setTodoForToday] = React.useState({
-    tite: "Gym jana hai taanu",
-    description: "Shaam 6 - 7",
-    id: 1,
-  });
+  const [todoForToday, setTodoForToday] = React.useState([])
 
-  setTimeout(() => {
-    setTodoForToday({
-      tite: "Gym jana hai taanu",
-      description: "Shaam 6 -564",
-      id: Math.random() * 1000,
-    });
-  }, 1000);
+  React.useEffect(() => {
+    fetch("http://localhost:3000/todos", {
+      method: "GET",
+    })
+      .then((res) => res.json().then((data) => setTodoForToday(data)));
+  }, []);
+
+  // setTimeout(() => {
+  //   setTodoForToday({
+  //     tite: "Gym jana hai taanu",
+  //     description: "Shaam 6 -564",
+  //     id: Math.random() * 1000,
+  //   });
+  // }, 1000);
 
   return (
     <div>
