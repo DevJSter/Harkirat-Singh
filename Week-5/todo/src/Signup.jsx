ac58a167d9eae420edd6f5d7eec12f0bfaa5511a
Shubham Tiwari <shubht3303@gmail.com>
2024-03-30 01:19:18 +0530

    rim

--- Week-5/todo/src/Signup.jsx
+++ Week-5/todo/src/Signup.jsx
@@ -1,26 +1,46 @@
 import * as React from "react";
-// import Stack from "@mui/material/Stack";
 import Button from "@mui/material/Button";
-// import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardActions from "@mui/material/CardActions";
+import Typography from "@mui/material/Typography";
+
 const Signup = () => {
   return (
-    <div className="h-full mt-15 object-center bg-gray-100">
-      <TextField
-        id="filled-basic"
-        label="Enter Your Username"
-        variant="filled"
-      />
-      <br />
-      <TextField
-        id="filled-basic"
-        label="Enter Your Password"
-        variant="filled"
-      />
-      <br />
-      <Button variant="contained" className="">
-        Submit
-      </Button>
+    <div className="flex justify-center items-center h-screen bg-gray-100">
+      <Card variant="outlined">
+        <CardContent>
+          <Typography
+            className="flex justify-center items-center"
+            sx={{ fontSize: 23 }}
+            color="text.secondary"
+            gutterBottom
+          >
+            Welcome To Coursera ! Sign Up below
+          </Typography>
+          <TextField
+            id="username"
+            label="Enter Your Username"
+            variant="filled"
+            fullWidth
+            className="mb-4"
+          />
+          <TextField
+            id="password"
+            label="Enter Your Password"
+            variant="filled"
+            type="password"
+            fullWidth
+            className="mb-6"
+          />
+        </CardContent>
+        <CardActions>
+          <Button variant="contained" color="primary">
+            Submit
+          </Button>
+        </CardActions>
+      </Card>
     </div>
   );
 };
