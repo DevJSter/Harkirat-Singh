42ad0a49aed0f308902e2599f0beab51f9178188
Shubham Tiwari <shubht3303@gmail.com>
2024-03-30 11:43:25 +0530

    rim

--- Week-5/todo/src/App.js
+++ Week-5/todo/src/App.js
@@ -3,10 +3,13 @@ import React from 'react';
 import './App.css';
 import Signup from './Signup';
 import SignIn from './SignIn';
+import AppBar from './AppBar';
+// import { AppBar } from '@mui/material';
 
 function App() {
   return (
     <div className="App">
+      <AppBar />
      <Router>
       <Routes>
         <Route path="Signup" element={<Signup />}/>
