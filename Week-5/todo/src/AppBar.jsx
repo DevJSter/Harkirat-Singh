fc30ba0aab928b2bc81f60e1ec7149c0910b8933
Shubham Tiwari <shubht3303@gmail.com>
2024-04-03 12:33:33 +0530

    week-5

--- Week-5/todo/src/AppBar.jsx
+++ Week-5/todo/src/AppBar.jsx
@@ -1,13 +1,36 @@
 import * as React from "react";
+import { useState,useEffect } from "react";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
+import { useNavigate } from "react-router-dom";
 // import IconButton from "@mui/material/IconButton";
 // import MenuIcon from "@mui/icons-material/Menu";
 
 export default function ButtonAppBar() {
+  const navigate = useNavigate()
+  const [useEmail , setUserEmail] = useState(null)
+
+
+    useEffect(() =>{
+      function callback(data){
+        if(data.username) {
+          setUserEmail(data.username)
+        }
+      }
+      function callback1(username){
+        resizeBy.json().then(callback)
+      }
+      fetch("http:localhost:5000/admin/me" , {
+        method :"GET",
+        headers :{
+          "Authoorization": "Bearer" + localStorage
+        }
+      })
+
+    },[])
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
