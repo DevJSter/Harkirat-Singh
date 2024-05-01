29fd78eb047fd6935cc1a72313c79208107cbe47
Shubham Tiwari <shubht3303@gmail.com>
2024-05-01 12:50:28 +0530

    incremenet

---
+++ Week-5/counter/src/App.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+
+const App = () => {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    setCount(count - 1);
+  };
+
+  return (
+    <div>
+      <button onClick={increment}>Increment</button>
+      <h1>{count}</h1>
+      <button onClick={decrement}>Decrement</button>
+    </div>
+  );
+};
+
+export default App;
