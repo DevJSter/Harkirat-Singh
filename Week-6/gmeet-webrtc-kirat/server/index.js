14e2e394de9bb6a8ef80fd6e92626539624b2767
Shubham Tiwari <shubht3303@gmail.com>
2024-05-07 00:03:09 +0530

    server

--- Week-6/gmeet-webrtc-kirat/server/index.js
+++ Week-6/gmeet-webrtc-kirat/server/index.js
@@ -3,7 +3,6 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
-
 const io = new Server(server, {
   cors: {
     origin: "http://localhost:5173"
