053f9785e61da1175ea20fccfc8219f4bf24638d
Shubham Tiwari <shubht3303@gmail.com>
2024-05-07 13:18:44 +0530

    MeetingView.jsx

---
+++ Week-6/gmeet-webrtc-kirat/client/src/components/MeetingView.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+const MeetingView = () => {
+  const [state,setState] = useState("");
+    return (
+    <div>
+    
+    </div>
+  )
+}
+
+export default MeetingView
