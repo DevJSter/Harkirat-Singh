4233dce834d7811e414cb5f11028929c72512a40
Shubham Tiwari <shubht3303@gmail.com>
2024-07-01 02:24:26 +0530

    OpenAPi -Sec // Decorators

---
+++ Week12/Week12.1/todoBackend/src/todos/todoService.ts
@@ -0,0 +1,24 @@
+import { Todo } from "./todo";
+
+export type TodoCreationParams = Pick<Todo, "title" | "description">;
+
+
+export class TodoService {
+    public get(todoId: string) : Todo{
+        return {
+            id : todoId,
+            title : "mocked todo",
+            description : "Mocked Desc";
+            done:false;
+        }
+    }
+    public creat(TodoCreationParams : TodoCreationParams) : Todo {
+        console.log("Mocked todo call");
+        return {
+            id : todoId,
+            title : "mocked todo",
+            description : "Mocked Desc";
+            done:false;
+        }
+    }
+}
---
+++ Week12/Week12.1/todoBackend/src/todos/todosService.ts
@@ -0,0 +1,26 @@
+import {
+  Body,
+  Controller,
+  Get,
+  Path,
+  Post,
+  Query,
+  Route,
+  SuccessResponse,
+} from "tsoa";
+
+import {Todo} from "./todo";
+import { TodoCreationParams,TodoService } from "./todoService";
+
+ 
+@Route("todo")
+export class TodoConroller extends Controller {
+
+        @Get("{todoID{")
+        public async getTodo(
+            @Path() todoId : string;
+        )
+        {
+            let todoService = new TodoService();
+            return todoService.get(todoId)
+        }   
